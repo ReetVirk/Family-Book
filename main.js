44409051c52f211f74dc0a372d0f3ce639863e12
ReetVirk <85968565+ReetVirk@users.noreply.github.com>
2022-03-04 18:24:45 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 3
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = ["https://i.postimg.cc/JnL6wtrd/sister.jpg","https://i.postimg.cc/5ymDKL83/bro.jpg","https://i.postimg.cc/bw5W5zSK/mother.jpg","https://i.postimg.cc/wjMnFtMX/father.jpg"];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = ["Reet","Gavin","Kavi","Gurpreet"];
+ 
+    document.getElementById("family_member_image").src = updatedImage[i];
+    document.getElementById("family_member_name").innerHTML = updatedName[i];
+}
